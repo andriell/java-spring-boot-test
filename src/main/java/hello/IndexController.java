bccd417a990cf890873a27e5e6f30dcb78fa8d8f
andriell <root.andriell@gmail.com>
2017-05-25 13:12:41 +0300

    start

--- src/main/java/hello/IndexController.java
+++ src/main/java/hello/IndexController.java
@@ -3,11 +3,12 @@ package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/")
 public class IndexController {
-
-    @RequestMapping(value="/", method= RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index";
     }
